fa56efd14cb8ede1f5a8e4905b0d44cd1ee35d2d
max103 <max@bell.camp>
2021-04-25 10:43:59 -0700

    Added MP1 folder and capacitive touch demo script

---
+++ MP1/Capacitive_Sensor_Testing/Capacitive_Sensor_Testing.ino
@@ -0,0 +1,39 @@
+#include <CapacitiveSensor.h>
+
+/*
+ * CapitiveSense Library Demo Sketch
+ * Paul Badger 2008
+ * Uses a high value resistor e.g. 10M between send pin and receive pin
+ * Resistor effects sensitivity, experiment with values, 50K - 50M. Larger resistor values yield larger sensor values.
+ * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
+ */
+
+
+CapacitiveSensor  cs_4_2 = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+
+void setup()                    
+{
+   pinMode(13, OUTPUT);
+   Serial.begin(9600);
+}
+
+void loop()                    
+{
+    long start = millis();
+    long total1 =  cs_4_2.capacitiveSensor(30);
+
+    Serial.print(millis() - start);        // check on performance in milliseconds
+    Serial.print("\t");                    // tab character for debug windown spacing
+
+    Serial.println(total1);                  // print sensor output 1
+    if(total1 > 1000) {
+      
+       int mapped = map(total1, 0, 10000, 0, 255);
+      analogWrite(13, mapped);
+    
+    } else {
+      analogWrite(13, 0);
+    }
+    
+    delay(10);                             // arbitrary delay to limit data to serial port 
+}
